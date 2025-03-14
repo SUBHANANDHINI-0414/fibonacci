fa44e274e05883f953b6af42dbbfa7379e60bd07
SUBHANANDHINI-0414 <subhanandhinivs@gmail.com>
2025-03-14 13:58:37 +0530

    Update bubblesort.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
-
-public class Fibonacci {
+public class BubbleSort {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of terms: ");
-        int n = scanner.nextInt();
-        scanner.close();
-        
-        int first = 0, second = 1;
-        System.out.println("Fibonacci Series:");
-        
-        for (int i = 0; i < n; i++) {
-            System.out.print(first + " ");
-            int next = first + second;
-            first = second;
-            second = next;
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        bubbleSort(arr);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
         }
     }
 }
